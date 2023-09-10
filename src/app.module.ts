ecd0a5b0afd78a12d01b88cdf80fcf3a713727bd
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-09-09 23:56:20 -0500

    [imp] (POST, PUT, DELETE, PATCH) ✅

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
+import { BrandsController } from './controllers/brands.controller';
+import { UsersController } from './controllers/users.controller';
+import { CostumersController } from './controllers/costumers.controller';
+import { OrdersController } from './controllers/orders.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  controllers: [AppController, ProductsController, CategoriesController, BrandsController, UsersController, CostumersController, OrdersController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -8,7 +18,9 @@ export class ProductsController {
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `products: limit => ${limit} offset => ${offset} brand => ${brand}`;
+    return {
+      message: `products: limit => ${limit} offset => ${offset} brand => ${brand}`,
+    };
   }
   // http://127.0.0.1:3000/products?limit=50&offset=12&brand=xyz
 
@@ -23,4 +35,36 @@ export class ProductsController {
     return `product ${productId}`;
   }
   // http://127.0.0.1:3000/products/one
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'accion de crear',
+      payload,
+    };
+  }
+  // http://localhost:3000/products
+
+  //put update all the fields
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return `product ${id} deleted`;
+  }
+
+  //patch only update the fields that we send
+  @Patch(':id')
+  patch(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
 }
