c9873f4a1665dca228ca8676f5bc8fd14aea0823
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-09-09 23:12:36 -0500

    [add] :: Query routes / order Static → Dinamic Routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,10 +18,28 @@ export class AppController {
   hello() {
     return 'con /sas/';
   }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `yo soy un filter`;
+  }
+  // http://127.0.0.1:3000/products/filter
+
   @Get('products/:productId')
   getProduct(@Param('productId') productId: string) {
     return `product ${productId}`;
   }
+  // http://127.0.0.1:3000/products/one
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products: limit => ${limit} offset => ${offset} brand => ${brand}`;
+  }
+  // http://127.0.0.1:3000/products?limit=50&offset=12&brand=xyz
 
   @Get('categories/:id/products/:productId')
   getCategories(
@@ -30,4 +48,5 @@ export class AppController {
   ) {
     return `product ${productId} and category ${id}`;
   }
+  // http://127.0.0.1:3000/categories/1/products/2
 }
