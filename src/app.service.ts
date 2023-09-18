4e83585f1e159838a11346f78619c8ba351ad742
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-09-18 17:42:27 -0500

    [imp] (config Typing) ✅ :: typing enviroments

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,24 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { /* ConfigService, */ ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
+    // private configService: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
     @Inject('API_KEY') private readonly apiKey: string,
     @Inject('GLOBAL_VALUE') private readonly globalValue: string,
     @Inject('TASKS') private tasks: TestTask[],
-    private configService: ConfigService,
   ) {}
   getHello(): string {
-    const env = this.configService.get<string>('DATABASE_NAME');
-    const env_key = this.configService.get<string>('API_KEY');
+    // const env = this.configService.get<string>('DATABASE_NAME');
+    // const env_key = this.configService.get<string>('API_KEY');
+    const env_db = this.configService.database.name;
+    const env_key = this.configService.apiKey;
     const inject_keys = this.apiKey;
     const keys = { env_key, inject_keys };
-    const message = `Hello World! ${this.globalValue} :: Database: ${env}`;
+    const message = `Hello World! ${this.globalValue} :: Database: ${env_db}`;
     const tasks = this.tasks;
     return JSON.stringify({ keys, message, tasks });
   }
