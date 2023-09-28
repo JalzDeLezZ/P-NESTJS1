124dc09d372fd2433706b1bdec47a724c9dc2c53
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-09-28 00:05:01 -0500

    [imp] (Custom Decorator) :: set public routes ✅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,27 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { Public } from './auth/decorators/public.decorator';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseGuards(ApiKeyGuard)
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  //! @SetMetadata('isPublic', true) //1st way
+  @Public() //! 2nd way
   @Get('test')
   newEndpoint() {
     return 'This is a test';
   }
 
+  //? set as a public route
+  @Public()
   @Get('tasks')
   getTasks() {
     return this.appService.getTasks();
--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -6,12 +6,22 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    // const isPublic = this.reflector.get('isPublic', context.getHandler());
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+
+    if (isPublic) {
+      return true;
+    }
+
     const requert = context.switchToHttp().getRequest<Request>();
     const authHeader = requert.header('Auth');
     const isAuth = authHeader === 'test';
