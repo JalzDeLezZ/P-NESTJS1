782cf4b6603641a0e7765063feb598ed249375ef
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-10-09 23:00:41 -0500

    [upd] (UseGuard with JwtAuthGuard && Set public routes) :: ✅

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -22,14 +22,16 @@ import {
 
 import { ProductsService } from './../services/products.service';
 import { MongoIdPipe } from 'src/common/mongo-id.pipe';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Public } from 'src/auth/decorators/public.decorator';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
+  @Public()
   @Get()
   @ApiOperation({ summary: 'List of products' })
   async getProducts(@Query() params: FilterProductsDto) {
@@ -44,6 +46,7 @@ export class ProductsController {
     return `yo soy un filter!!!`;
   }
 
+  @Public()
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', MongoIdPipe) productId: string) {
